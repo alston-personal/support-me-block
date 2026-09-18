db07a4aa20fd23c70787f53f6af295c066d41ba3
Sunlake <register@milkcat.org>
2026-09-18 08:36:28 +0800

    feat: export project utility kit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export {default as SupportMeBlock} from './SupportMeBlock';
+export type {SupportMeBlockProps} from './SupportMeBlock';
+export {default as FeedbackBlock} from './FeedbackBlock';
+export type {FeedbackBlockProps, ProjectFeedbackCategory, ProjectFeedbackContext} from './FeedbackBlock';
+export {default as ProjectUtilityDock} from './ProjectUtilityDock';
+export type {ProjectUtilityDockProps} from './ProjectUtilityDock';
